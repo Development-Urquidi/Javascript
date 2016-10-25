acb437587c1176151f7a219c1021152b67384be1
Juan Carlos Urquidi <pro.urquidi@gmail.com>
2016-10-24 19:16:24 -0500

    Update EulerProblem1.js

--- EulerProblem1.js
+++ EulerProblem1.js
@@ -1,16 +1,10 @@
+// Problem statement: Find the sum of all values divisible by
+// 3 and/or 5 but less than 1,000.
+
 var value = (1);
 var sum = (0);
 while(value < 1000) {
-  if(value % 3 === 0){
-    sum = value + sum;
-    value = value + 1;
+  if(value % 3||5 === 0){sum = value + sum;}
+  value = value + 1;
   }
-  else{
-    if(value % 5 === 0){
-    sum = value + sum;
-    value = value + 1;
-  }
-  else{value = value + 1;}
-  }
-}
 if(value === 1000){console.log(sum);}
