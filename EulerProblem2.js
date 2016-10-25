a60b2ec86c57344f50f0a7acbfcbc36f78d722cf
Juan Carlos Urquidi <pro.urquidi@gmail.com>
2016-10-24 19:07:38 -0500

    Add files via upload

--- EulerProblem2.js
+++ EulerProblem2.js
@@ -1,16 +1,15 @@
-var prevalue = (0);
-var value = (1);
+//Problem statement: Find the sum of all EVEN Fibonacci numbers
+//between 1 and 4,000,000.
+
+var low = (0);
+var mid = (0);
+var high = (1);
 var sum = (0);
-while(value <= 4000000){
-	if(value % 2 === 0){
-		sum = sum + value;
-		prevalue = prevalue + value;
-		value = value + prevalue;
+while(high <= 4000000)
+{
+	if(high % 2 === 0) {sum = sum + high;}
+		low = mid;
+		mid = high;
+		high = mid + low;
 	}
-	else {
-	   sum = sum;
-	   prevalue = value + prevalue;
-	   value = value + prevalue;
-       }
-		}
-if(value >= 4000000){console.log = sum;}
+if(high > 4000000){console.log(sum)}
